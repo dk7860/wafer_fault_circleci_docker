2e1a9e669d0fb2bc2bac074f7ab2d1d4d25e527a
DANISH_KHAN <dk74433@gmail.com>
2021-12-08 18:12:46 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="wafer-fault-detection",
+    version="0.0.3",
+    description="ML project",
+    author="Shivansh jayara", 
+    packages=find_packages(),
+    license="MIT"
+)
